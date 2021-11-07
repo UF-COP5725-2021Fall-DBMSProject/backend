3602176e998729c8acf01f78614838f92ebd8b38
chouhy <jimchou1995@gmail.com>
2021-11-08 04:25:11 +0800

    generate c1a template

--- apiServer.py
+++ apiServer.py
@@ -1,6 +1,25 @@
 from flask import Flask
 from flask import jsonify
 from flask import Blueprint
+
+# testing
+import json
+def _getData(file):
+    with open(file) as f:
+        data = json.load(f)
+    result = {}
+    columns = []
+
+    for d in data["schema"]["fields"]:
+        if d["name"] != "index":
+            columns.append(d["name"])
+    for d in data["data"]:
+        del d["index"]
+
+    result["columns"] = columns
+    result["data"] = data["data"]
+    return {"result":result}
+# /testing
 app = Flask(__name__)
 @app.route('/api/', methods=['GET', 'POST'])
 def api():
@@ -14,6 +33,15 @@ def example_year(year):
     return jsonify({'example':year})
 
 
+
+c1_bp = Blueprint('C1',__name__)
+
+
+@c1_bp.route('/funcA/<string:name>')
+def c1a(name):
+    return _getData('./hy.json')
+
 if __name__ == '__main__':
     app.register_blueprint(example_bp, url_prefix='/example')
+    app.register_blueprint(c1_bp, url_prefix='/c1')
     app.run(host='0.0.0.0', port=8000)
