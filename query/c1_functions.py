2777bd75f4f0cf40f480053e1cb7e5247c1954b8
Yi-Ming Chang <69064626+yimingStar@users.noreply.github.com>
2021-11-22 12:28:13 -0500

    comment the conflix header

--- query/c1_functions.py
+++ query/c1_functions.py
@@ -154,19 +154,19 @@ def c1_function_b(driverId, query_engine=engine):
 
     return compare_first_three_years, similarity_of_first_three_years
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+# <<<<<<< HEAD
+# <<<<<<< HEAD
+# # json_all_result = c1_function_b(2)
+# # print(json_all_result)
+# =======
 # json_all_result = c1_function_b(2)
-# print(json_all_result)
-=======
-json_all_result = c1_function_b(2)
-#print(json_all_result)
->>>>>>> update c1_function, todo:choose most competitive d
-=======
+# #print(json_all_result)
+# >>>>>>> update c1_function, todo:choose most competitive d
+# =======
 
-# json_all_result = c1_function_b(2)
-# print(json_all_result)
->>>>>>> change c1b return structure
+# # json_all_result = c1_function_b(2)
+# # print(json_all_result)
+# >>>>>>> change c1b return structure
 
 
 def c1_function_c(driverId, query_engine=engine):
